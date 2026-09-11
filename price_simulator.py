a660a9131f0b4c900cd57e3d93ebcf9f6f476217
alonlevy937 <al@mail.tau.ac.il>
2026-09-11 15:45:41 +0300

    fixed a bug

--- price_simulator.py
+++ price_simulator.py
@@ -11,10 +11,10 @@ def generate_price_path(start_price: float, num_ticks: int, step_size: float) ->
 def maybe_generate_incoming_order(fair_price:float,bid_price: float, ask_price: float, act: str ) -> tuple[str, float] | None:
     x=random.random()
     if act=="BUY":
-        if x< 0.05*(math.e**(-k*(abs(ask_price-fair_price)))):
+        if x< 0.05*(math.e**(-k*(ask_price-fair_price))):
             return act, ask_price
     if act == "SELL":
-        if x < 0.05 * (math.e ** (-k * (abs(bid_price - fair_price)))):
+        if x < 0.05 * (math.e ** (-k * (fair_price-bid_price))):
             return act, bid_price
 
 
